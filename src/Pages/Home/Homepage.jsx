447cf7a3f1c068e0b428986c0e835a0041aabd42
Asad9340 <asadulimran1999@gmail.com>
2024-05-09 22:04:25 +0600

    newsletter section added successfully

--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner/Banner";
 import Map from "../../components/Map/Map";
+import Newsletter from "../../components/Newsletter/Newsletter";
 
 function Homepage() {
   return (
     <div className="my-8 md:my-10 lg:my-14 space-y-8 md:space-y-12 lg:space-y-16">
       <Banner />
-      <Map/>
+      <Map />
+      <Newsletter/>
     </div>
   );
 }
