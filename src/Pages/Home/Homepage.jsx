8b8de8d54852ec3df06857769a08badb56070aa9
Asad9340 <asadulimran1999@gmail.com>
2024-05-13 02:33:36 +0600

    added special offer and modal on the homepage

--- src/Pages/Home/Homepage.jsx
+++ src/Pages/Home/Homepage.jsx
@@ -1,10 +1,21 @@
+import { useEffect } from "react";
 import Banner from "../../components/Banner/Banner";
 import FeaturedRooms from "../../components/FeatuedRooms/FeaturedRooms";
 import Map from "../../components/Map/Map";
 import Newsletter from "../../components/Newsletter/Newsletter";
 import UserReviews from "../../components/UserReviews/UserReviews";
+import Swal from "sweetalert2";
 
 function Homepage() {
+  useEffect(() => {
+Swal.fire({
+  imageUrl:
+    'https://png.pngtree.com/png-vector/20221107/ourmid/pngtree-up-to-20-off-price-tag-design-png-image_6429660.png',
+  imageHeight: 300,
+  imageWidth: 400,
+  imageAlt: 'A tall image',
+});
+  },[])
   return (
     <div className="my-8 md:my-10 lg:my-14 space-y-8 md:space-y-12 lg:space-y-16">
       <Banner />
